c07ef8f8914e98f57081212fe9d5e7be8351eaeb
yumin-jung <jungym887@naver.com>
2022-12-24 13:30:55 +0900

    Update local server to deployed internet server

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch("http://localhost:4000", {
+  const response = await fetch("https://chatgpt-js.onrender.com", {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
